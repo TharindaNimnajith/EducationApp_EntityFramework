51920104b0407eee434e1de8f92d60cc82b00d90
Tharinda <tharindarajapakshe@gmail.com>
2020-07-06 23:22:07 +0530

    create console project

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EducationApp_EntityFramework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //
+        }
+    }
+}
